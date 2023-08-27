4b6a23e0fbd1936db894d85257d38334bf38d39d
Felix Ocker <felix.ocker@googlemail.com>
2023-08-27 12:05:23 +0200

    chore: move paths to constants

--- german_law_bot/constants.py
+++ german_law_bot/constants.py
@@ -4,12 +4,11 @@ import os
 from chromadb.utils import embedding_functions
 
 
-LAWS = {
-    # "ESTG_XML": "https://www.gesetze-im-internet.de/estg/xml.zip",
-    "BGB_XML": "https://www.gesetze-im-internet.de/bgb/xml.zip",
-}
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 EMBEDDING_MODEL = "text-embedding-ada-002"
+
+CONFIG = "config.yaml"
+DOWNLOADS = "../data/downloads/"
 
 OPENAI_EF = embedding_functions.OpenAIEmbeddingFunction(
     api_key=OPENAI_API_KEY,
